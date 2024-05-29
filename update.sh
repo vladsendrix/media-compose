ec56d6698d9914a541a1cf28201aa8dc36e5b4ba
vladsendrix <vladsendrix@gmail.com>
2024-05-29 12:38:31 +0000

    update script to back up configs before updating containers

--- update.sh
+++ update.sh
@@ -1,2 +1,26 @@
+#!/bin/bash
+
+CONFIG_DIR="../configs"
+
+ORIGINAL_DIR=$(pwd)
+
+cd $CONFIG_DIR || { echo "Failed to change directory to $CONFIG_DIR"; exit 1; }
+
+git add .
+DATE_TIME=$(date '+%Y-%m-%d %H:%M:%S')
+git commit -m "Backup before Docker update on $DATE_TIME"
+
+if [ $? -ne 0 ]; then
+    echo "Git commit failed. Exiting update script."
+    exit 1
+fi
+
+cd $ORIGINAL_DIR || exit
+
 docker compose pull
+
 docker compose up -d
+
+docker image prune -f
+
+echo "Docker containers updated successfully."
